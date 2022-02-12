347762831d112e5e388ba5a4a85442d2320d4150
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-02-12 20:12:56 +0100

    feat:add ervice and repository files

--- src/di/serviceLocator.ts
+++ src/di/serviceLocator.ts
@@ -1,4 +1,5 @@
 import ImageModel, { Image } from '../models/Image';
+import ImageTagModel, { ImageTag } from '../models/ImageTag';
 import UserModel, { User } from '../models/User';
 import Repository from '../repository/Repository';
 import AuthService from '../services/AuthService';
@@ -6,5 +7,6 @@ import AuthService from '../services/AuthService';
 export default class ServiceLocator {
   static imageRepository: Repository<Image> = new Repository(ImageModel);
   static userRepository: Repository<User> = new Repository(UserModel);
+  static imageTagRepository: Repository<ImageTag> = new Repository(ImageTagModel);
   static authService: AuthService = new AuthService(this.userRepository);
 }
